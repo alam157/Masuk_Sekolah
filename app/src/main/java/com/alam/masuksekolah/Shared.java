4fe35b20c72dea209787a763e44f2cf232480962
Dawaam <dawaamsuruuri17@gmail.com>
2015-11-06 02:29:14 +0700

    Shared.java

---
+++ app/src/main/java/com/alam/masuksekolah/Shared.java
@@ -0,0 +1,21 @@
+package com.alam.masuksekolah;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Dawaam on 06/11/2015.
+ */
+public class Shared extends AppCompatActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.card_share);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
